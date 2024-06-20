bacafdfbe1f33aa25bafe76ff6f88070aef79b77
OlegKo2024 <4oki66@gmail.com>
2024-06-20 17:33:35 +0300

    Add files via upload

---
+++ LectureVar.py
@@ -0,0 +1,10 @@
+print('Lesson ''20.06.2024')
+name = 'Oleg'
+print(name)
+name = 'Nikita'
+print(name) # берет последнюю переменную
+date_of_birth = '30.10.1966' # Snake case метод
+print(date_of_birth)
+print('my ''birthday ''is',date_of_birth)
+DateOfBirth = '04.11.2004' # Camel case
+print(name, 'birthday ''is',DateOfBirth)
---
+++ PracticeVar.py
@@ -0,0 +1,9 @@
+print('Variables')
+Number_of_homeworks_done = 12
+Number_of_hours_spent = 1.5
+Minutes_per_hour = 60
+Course_name = 'Python'
+Rate_per_homework = 'Speed'
+Speed = Number_of_hours_spent * Minutes_per_hour // Number_of_homeworks_done
+print(Course_name,'takes',Speed, 'per ''homework')
+
