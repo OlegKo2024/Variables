b33e87b63ed597b81ab5cc8c167812161befb3b6
OlegKo2024 <4oki66@gmail.com>
2024-06-21 10:30:27 +0300

    Add files via upload

--- PracticeVar.py
+++ PracticeVar.py
@@ -7,3 +7,6 @@ Rate_per_homework = 'Speed'
 Speed = Number_of_hours_spent * Minutes_per_hour // Number_of_homeworks_done
 print(Course_name,'takes',Speed, 'per ''homework')
 
+# Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+Speed = Number_of_hours_spent / Number_of_homeworks_done
+print('Курс:',Course_name + ',','всего задач:',str(Number_of_homeworks_done)+',','затрачено часов:',str(Number_of_hours_spent) + ',','среднее время выполнения',Speed,'часа.')
